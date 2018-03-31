781775f3b1904fbe3e25413429bb1c8e18b8e0cd
Danila <fourlessthanthree@yahoo.com>
2018-03-31 23:57:02 +0300

    Add client side reload, rework plugin creation process

--- README.md
+++ README.md
@@ -19,21 +19,47 @@ npm install --save-dev electron-reload-webpack-plugin
 
 ```
 const path = require('path');
-const ElectronReloadWebpackPlugin = require('electron-reload-webpack-plugin');
+const createElectronReloadWebpackPlugin = require('electron-reload-webpack-plugin');
+
+// create one plugin for both renderer and main process
+const ElectronReloadWebpackPlugin = createElectronReloadWebpackPlugin({
+    // Path to main process file
+    path: path.join(__dirname, './build/backend.js'),
+    // Other 'electron-connect' options
+    logLevel: 0
+});
 
 module.exports = {
     // ...
 
+    // Target should be 'electron-main' or 'electron-renderer'
+    // Usually you will have two webpack configs, one for renderer and other one for main process
+    target: 'electron-renderer',
+
     plugins: [
         // ...
-        new ElectronReloadWebpackPlugin({
-            path: path.join(__dirname, 'dist'),
-            logLevel: 0
-        })
-    ]
+        ElectronReloadWebpackPlugin()
 
+        // You can also pass target like this `ElectronReloadWebpackPlugin('electron-renderer')`, if you don't have it in config
+    ],
     // ...
 };
+```
+
+### 1.1 Create `electron-connect` client if you want to reload renderer process
+
+```
+import { app, BrowserWindow } from 'electron';
+import { client } from 'electron-connect';
+
+app.on('ready', () => {
+    const mainWindow = new BrowserWindow({
+        // ...
+    });
+
+    // Pass your BrowserWindow object
+    client.create(mainWindow);
+});
 ```
 
 ### 2. Start webpack with watch option
--- src/index.js
+++ src/index.js
@@ -1,22 +1,54 @@
 import { server as electronConnectServer } from 'electron-connect';
 
-class ElectronReloadWebpackPlugin {
-    constructor(options) {
-        this.options = options;
+const TARGET = {
+    main: 'electron-main',
+    renderer: 'electron-renderer'
+};
+const AVAILABLE_TARGETS = [TARGET.main, TARGET.renderer];
+
+function createElectronReloadWebpackPlugin(options = {}) {
+    let server = null;
+
+    function restartOrReloadServer(targetType, server) {
+        if (targetType === TARGET.main) {
+            server.restart();
+        } else {
+            server.reload();
+        }
     }
 
-    apply(compiler) {
-        let server;
+    function applyCompiler(userTargetType) {
+        return {
+            apply(compiler) {
+                const configTarget = AVAILABLE_TARGETS.find(target => target === compiler.options.target);
+                const targetType = userTargetType || configTarget || TARGET.main;
 
-        compiler.hooks.done.tap('ElectronReloadWebpackPlugin', () => {
-            if (!server) {
-                server = electronConnectServer.create(this.options);
-                server.start();
-            } else {
-                server.restart();
+                compiler.hooks.done.tap('ElectronReloadWebpackPlugin', () => {
+                    if (!server) {
+                        server = electronConnectServer.create(options);
+                        server.start();
+                    } else {
+                        restartOrReloadServer(targetType, server);
+                    }
+                });
             }
-        });
+        };
     }
+
+    return target => {
+        if (target && !AVAILABLE_TARGETS.includes(target)) {
+            console.log(
+                `\nElectronReloadWebpackPlugin: Specified target should be one of "${AVAILABLE_TARGETS.join(
+                    ' | '
+                )}". You passed "${target}"`
+            );
+            console.log(`ElectronReloadWebpackPlugin: Using default target "${TARGET.main}"`);
+
+            return applyCompiler(TARGET.main);
+        } else {
+            return applyCompiler();
+        }
+    };
 }
 
-export default ElectronReloadWebpackPlugin;
+export default createElectronReloadWebpackPlugin;
